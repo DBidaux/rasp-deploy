ab0e363f4d8b09da613dd20bba86489cd2e4f4cb
Didier <bidaux3@hotmail.es>
2024-07-07 15:46:39 +0200

    testing

--- client/src/setttings.js
+++ client/src/setttings.js
@@ -1,5 +1,5 @@
 //cambiar por tu url o ip que te de el hosting o la ip de la rasp
 
-const API_URL = "https://cuadrillagolfes.es/api";
+const API_URL = "https://localhost:8080";
 
 export default API_URL;
