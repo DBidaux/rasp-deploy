433ec3bb9d012ce6848711498954bd2584e79d09
Didier <bidaux3@hotmail.es>
2024-07-08 12:17:14 +0200

    testing

--- client/src/components/LandingPage/LandingPage.js
+++ client/src/components/LandingPage/LandingPage.js
@@ -10,15 +10,15 @@ export default function LandingPage() {
 			<NavBar></NavBar>
 			<div className="background">
 				<div className="content">
-					<h1 className="h1">Bienvenid@ Golf@!</h1>
+					<h1 className="h1">Bienvenid@!</h1>
 					<p className="p-2">
 						En ésta página puedes meter las próximas cenas/eventos
-						de la cuadrilla, así como consultar los gastos/facturas
-						y las diferentes compras!
+						con tus amigos, así como consultar los gastos/facturas y
+						las diferentes compras en común!
 					</p>
 					<p className="pt-2">
-						Toca el día, rellena el formulario, guarda yyy
-						¡Voilá! ya tienes creado tu evento.
+						Toca el día, rellena el formulario, guarda yyy ¡Voilá!
+						ya tienes creado tu evento.
 					</p>
 					<Calendario></Calendario>
 				</div>
